5ff90b952e3bd0d15768a10f626a705323731d27
Charles Smith <chas@chasmac.local>
2019-11-07 15:12:35 +1100

    Beacon errors and changes

--- src/beacon/beacon.go
+++ src/beacon/beacon.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"strings"
 
 	"sarflags"
 )
@@ -15,51 +16,97 @@ type Beacon struct {
 	eid       string
 }
 
-// Make - Construct a beacon - return byte slice of frame
-func (b *Beacon) Make(header uint32, eid string, freespace uint64) ([]byte, error) {
+// New - Construct a beacon - return byte slice of frame
+func (b *Beacon) New(flags string, eid string, freespace uint64) error {
 	var err error
 
 	// Always present in a Beacon
-	if header, err = sarflags.Set(header, "version", "v1"); err != nil {
-		return nil, err
+	if b.header, err = sarflags.Set(b.header, "version", "v1"); err != nil {
+		return err
 	}
 	// And yes we are a Beacon Frame
-	if header, err = sarflags.Set(header, "frametype", "beacon"); err != nil {
-		return nil, err
+	if b.header, err = sarflags.Set(b.header, "frametype", "beacon"); err != nil {
+		return err
 	}
+
+	flags = strings.Replace(flags, " ", "", -1) // Get rid of extra spaces in flags
+
+	// Grab the flags and set the frame header
+	flag := strings.Split(flags, ",") // The name=val of the flag
+	for fl := range flag {
+		f := strings.Split(flag[fl], "=") // f[0]=name f[1]=val
+		switch f[0] {
+		case "descriptor", "stream", "txwilling", "rxwilling", "udplite", "freespace":
+			if b.header, err = sarflags.Set(b.header, f[0], f[1]); err != nil {
+				return err
+			}
+		case "freespaced":
+			// Make sure freespace has been turned on
+			if b.header, err = sarflags.Set(b.header, "freespace", "yes"); err != nil {
+				return err
+			}
+			switch f[1] {
+			case "d16", "d32", "d64":
+				if b.header, err = sarflags.Set(b.header, f[0], f[1]); err != nil {
+					return err
+				}
+
+			default:
+				es := "Beacon.New: Invalid freespaced " + f[1]
+				return errors.New(es)
+			}
+		default:
+			e := "Beacon.New: Invalid Flag " + f[0] + "=" + f[1] + " for Data Frame"
+			return errors.New(e)
+		}
+	}
+	b.freespace = freespace
+	b.eid = eid
+	return nil
+}
+
+// Put -- Encode the Saratoga Data Frame buffer
+func (b Beacon) Put() ([]byte, error) {
 
 	var frame []byte
 
-	pos := 0
-	b.header = header
-	b.freespace = freespace
-	b.eid = eid
-	if sarflags.GetStr(header, "freespace") == "yes" {
-		switch sarflags.GetStr(header, "freespaced") {
+	framelen := 4 + len(b.eid)
+	if sarflags.GetStr(b.header, "freespace") == "yes" {
+		switch sarflags.GetStr(b.header, "freespaced") {
 		case "d16":
-			frame = make([]byte, 4+2+len(eid))
-			binary.BigEndian.PutUint32(frame[:4], header)
-			binary.BigEndian.PutUint16(frame[4:6], uint16(freespace))
-			pos = 6
+			framelen += 2
 		case "d32":
-			frame = make([]byte, 4+4+len(eid))
-			binary.BigEndian.PutUint32(frame[:4], header)
-			binary.BigEndian.PutUint32(frame[4:8], uint32(freespace))
-			pos = 8
+			framelen += 4
 		case "d64":
-			frame = make([]byte, 4+4+len(eid))
-			binary.BigEndian.PutUint32(frame[:4], header)
-			binary.BigEndian.PutUint64(frame[4:12], uint64(freespace))
-			pos = 12
+			framelen += 8
 		default:
-			return frame, errors.New("Invalid Beacon Frame")
+			return nil, errors.New("Invalid Beacon Frame")
 		}
-		copy(frame[pos:], []byte(eid))
-		return frame, nil
 	}
-	frame = make([]byte, 4+len(eid))
-	binary.BigEndian.PutUint32(frame[:4], header)
-	copy(frame[4:], []byte(eid))
+
+	if framelen > sarflags.MaxFrameSize {
+		return nil, errors.New("Data - Maximum Frame Size Exceeded")
+	}
+	frame = make([]byte, framelen)
+
+	pos := 4
+	binary.BigEndian.PutUint32(frame[:pos], b.header)
+	if sarflags.GetStr(b.header, "freespace") == "yes" {
+		switch sarflags.GetStr(b.header, "freespaced") {
+		case "d16":
+			binary.BigEndian.PutUint16(frame[pos:6], uint16(b.freespace))
+			pos += 2
+		case "d32":
+			binary.BigEndian.PutUint32(frame[pos:8], uint32(b.freespace))
+			pos += 4
+		case "d64":
+			binary.BigEndian.PutUint64(frame[pos:12], uint64(b.freespace))
+			pos += 8
+		default:
+			return nil, errors.New("Invalid Beacon Frame")
+		}
+	}
+	copy(frame[pos:], []byte(b.eid))
 	return frame, nil
 }
 
