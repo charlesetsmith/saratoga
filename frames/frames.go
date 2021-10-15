39142c9d15d3b4ca5008e4b0dd1e957077722e45
Charles Smith <charlesetsmith@me.com>
2021-10-15 22:40:48 +1100

    Update returns with error codes for frames

--- frames/frames.go
+++ frames/frames.go
@@ -2,15 +2,20 @@
 
 package frames
 
+import (
+	"net"
+)
+
 // import "github.com/charlesetsmith/saratoga/holes"
 
 // Frame - Handler for different frames
 // 	beacon, data, metadata, request, status
 type Frame interface {
-	Encode() ([]byte, error)   // Put some type of frame
-	Decode(frame []byte) error // Get some type of frame
-	Print() string             // Print out contents of some type of frame
-	ShortPrint() string        // Quick summary print out of some type of frame
+	Encode() ([]byte, error)      // Put some type of frame
+	Decode([]byte) error          // Get some type of frame
+	Print() string                // Print out contents of some type of frame
+	ShortPrint() string           // Quick summary print out of some type of frame
+	UDPWrite(*net.UDPConn) string // "" is OK any other string is sent back on
 
 	/*
 		New() error
@@ -48,4 +53,17 @@ func Print(f Frame) string {
 // ShortPrint a frame into its structure via Frame interface
 func ShortPrint(f Frame) string {
 	return f.ShortPrint()
+}
+
+// Encode and Send a Frame to UDP Connection
+func UDPWrite(f Frame, conn *net.UDPConn) string {
+	var wframe []byte
+	var err error
+	if wframe, err = Encode(f); err != nil {
+		return "badpacket"
+	}
+	if _, err = conn.Write(wframe); err != nil { // And send it
+		return "cantsend"
+	}
+	return "success"
 }
