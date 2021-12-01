bd5ad51f02d79891e3cf95fed393c0c70c743faf
Charles Smith <charlesetsmith@me.com>
2021-12-02 07:53:54 +1100

    Added SO_REUSEADDR and SO_REUSEPORT to Muluticast setup

--- sarnet/sarnet.go
+++ sarnet/sarnet.go
@@ -60,15 +60,53 @@ func SetMulticastLoop(conn net.PacketConn, v4orv6 string) error {
 		if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_MULTICAST_LOOP, 1); err != nil {
 			return err
 		}
-		return nil
 	case "IPv6":
 		if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_IPV6, syscall.IPV6_MULTICAST_LOOP, 1); err != nil {
 			return err
 		}
-		return nil
 	default:
 		err := "invalid - should be IPv4 or IPv6:" + v4orv6
 		return errors.New(err)
 	}
-	//syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+		return err
+	}
+	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1); err != nil {
+		return err
+	}
+	return nil
 }
+
+/*
+func mcastOpen(bindAddr net.IP, port int, ifname string) (*ipv4.PacketConn, *net.UDPConn, error) {
+	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+		log.Fatal(err)
+	}
+	//syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
+	if err := syscall.SetsockoptString(s, syscall.SOL_SOCKET, syscall.SO_BINDTODEVICE, ifname); err != nil {
+		log.Fatal(err)
+	}
+
+	lsa := syscall.SockaddrInet4{Port: port}
+	copy(lsa.Addr[:], bindAddr.To4())
+
+	if err := syscall.Bind(s, &lsa); err != nil {
+		syscall.Close(s)
+		log.Fatal(err)
+	}
+	f := os.NewFile(uintptr(s), "")
+	c, err := net.FilePacketConn(f)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	u := c.(*net.UDPConn)
+	p := ipv4.NewPacketConn(c)
+
+	return p, u, nil
+}
+*/
